79eda1cfa06a2d20e85d19e7d361e929aa5707f8
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-04-07 14:27:23 -0400

    Code to print each question

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
 public class VariableApp {
+    public static void main(String[] args) {
+        //Question 1-5
+        String favoriteColor = "Grey";
+        System.out.println(favoriteColor);
+        int yearClassStarted = 2026;
+        System.out.println(yearClassStarted);
+        char middleInitial = 'B';
+        System.out.println(middleInitial);
+        boolean hasPets = true;
+        System.out.println(hasPets);
+        String niceSaying = "This too shall pass";
+        System.out.println(niceSaying);
+
+        //Step 2
+
+    }
 }
