cdf10e6af71117c4798ab020416971cb0e10151a
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-04-07 15:20:37 -0400

    small updates to make code more readable

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -5,12 +5,16 @@ public class VariableApp {
         //Question 1-5
         String favoriteColor = "Grey";
         System.out.println(favoriteColor);
+
         int yearClassStarted = 2026;
         System.out.println(yearClassStarted);
+
         char middleInitial = 'B';
         System.out.println(middleInitial);
+
         boolean hasPets = true;
         System.out.println(hasPets);
+
         String niceSaying = "This too shall pass";
         System.out.println(niceSaying);
 
@@ -19,6 +23,7 @@ public class VariableApp {
         double coffeePrice = 4.99;
         char favLetter = 'G';
         boolean isRaining = true;
+
         System.out.println("the answer for days in a week is " + daysInWeek);
         System.out.println("The answer for the price of coffee is " + coffeePrice);
         System.out.println("My favorite letter is " + favLetter);
