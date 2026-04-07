5fdb7d16953908ef5feab9e53ea058f3d6dbd6a3
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-04-07 15:13:24 -0400

    Step 3

---
+++ src/main/java/com/pluralsight/Reciept.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class Reciept {
+    public static void main(String[] args) {
+        //Variables
+        String itemName = "socks";
+        double itemPrice = 3.99;
+        int quantity = 4;
+        System.out.println("you bought " + quantity +" pairs of " + itemName + " for " + itemPrice);
+    }
+}
